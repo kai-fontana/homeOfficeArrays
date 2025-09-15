81e4cc45b559cf282243b86c1f28a06d1ae0eae6
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-09-15 08:26:45 -0300

    Add files via upload

---
+++ homeOfficeArrays-main/homeOffice/homeOfficeArrays/src/Main.java
@@ -0,0 +1,179 @@
+import classes.utilizadas.Book;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        //PRODUCT
+
+        classes.utilizadas.Product product01 = new classes.utilizadas.Product("Charger", 19.99);
+        classes.utilizadas.Product product02 = new classes.utilizadas.Product("Phone", 960.50);
+        classes.utilizadas.Product product03 = new classes.utilizadas.Product("Computer", 1200.90);
+        classes.utilizadas.Product product04 = new classes.utilizadas.Product("Mouse", 30.80);
+
+        //ArrayList
+        ArrayList<classes.utilizadas.Product> listOfProducts = new ArrayList<>();
+
+        //Adding elements to the ArrayList
+        listOfProducts.add(0, product01);
+        listOfProducts.add(1, product02);
+        listOfProducts.add(2, product03);
+        listOfProducts.add(3, product04);
+
+        //Showing the list
+        System.out.println("Full list: " + listOfProducts.toString());
+        System.out.println("There is: " + listOfProducts.size() + " products.");
+
+        classes.utilizadas.Product expensiveProduct = listOfProducts.get(0);
+
+        //Most expensive product 
+        for (classes.utilizadas.Product actualProduct : listOfProducts) {
+            if (actualProduct.getPrice() > expensiveProduct.getPrice()) {
+                expensiveProduct = actualProduct;
+
+            }
+        }
+        System.out.println("The most expensive product is: " + expensiveProduct);
+
+        //----------------------------------
+
+        //BOOKS
+
+        Book book01 = new Book("1984", "George Orwell" ,1949);
+        Book book02 = new Book("Pride and prejudice", "Jane Austen" ,1813);
+        Book book03 = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling" ,1997);
+
+        //ArrayList
+        ArrayList<Book> listOfBooks = new ArrayList<>();
+
+        listOfBooks.add(0, book01);
+        listOfBooks.add(1, book02);
+        listOfBooks.add(2, book03);
+
+        System.out.println("All books: " + listOfBooks);
+
+        //Books released after 2010
+        for (Book book : listOfBooks) {
+            if (book.getYearOfPublication() > 2010) {
+                System.out.println(book);
+            }
+        }
+
+        //----------------------------------
+
+        //STUDENTS
+        ArrayList<classes.utilizadas.Student> listOfStudents = new ArrayList<>();
+
+        classes.utilizadas.Student student01 = new classes.utilizadas.Student("Kai", 9.9);
+        classes.utilizadas.Student student02 = new classes.utilizadas.Student("James", 9.6);
+        classes.utilizadas.Student student03 = new classes.utilizadas.Student("Charles", 6.20);
+        classes.utilizadas.Student student04 = new classes.utilizadas.Student("Melanie", 3.80);
+
+        listOfStudents.add(0, student01);
+        listOfStudents.add(1, student02);
+        listOfStudents.add(2, student03);
+        listOfStudents.add(3, student04);
+
+        System.out.println("All students: " + listOfStudents.toString());
+
+        //Average note
+        double sumNotes = 0.0;
+        for (classes.utilizadas.Student student : listOfStudents) {
+            sumNotes += student.getGrades();
+        }
+        double averageGrades = sumNotes / listOfStudents.size();
+        System.out.println("The average grade is: " + averageGrades);
+
+        //Approved students
+        for (classes.utilizadas.Student student : listOfStudents) {
+            if (student.getGrades() >= 7.0) {
+                System.out.println("Aprovado(a): " + student);
+            }
+
+        //----------------------------------
+        //CARS
+        Car car01 = new Car("Onix", 2024, "White");
+        Car car02 = new Car("Uno", 2016, "Black");
+        Car car03 = new Car("Sedan", 2021, "Red");
+
+        ArrayList<Car> listOfCars = new ArrayList<>();
+
+        listOfCars.add(0, car01);
+        listOfCars.add(1, car02);
+        listOfCars.add(2, car03);
+
+        //Only red cars
+        for (Car car : listOfCars) {
+            if (car.getColor() == "Red") {
+                System.out.println(car);
+            }
+        }
+
+        //----------------------------------
+            // Traveling 
+        Travel travel01 = new Travel("South Korea", 18);
+        Travel travel02 = new Travel("Santa Maria City", 12);
+        Travel travel03 = new Travel("Tramandai Beach", 5);
+        Travel travel04 = new Travel("Cridreira Beach", 7);
+
+        ArrayList<Travel> listOfTravels = new ArrayList<>();
+
+        listOfTravels.add(0, travel01);
+        listOfTravels.add(1, travel02);
+        listOfTravels.add(2, travel03);
+        listOfTravels.add(3, travel04);
+
+        System.out.println("Full list: " + listOfTravels);
+
+        //Longer than 7 days
+        for (Travel travel : listOfTravels) {
+            if (travel.getDurationDays() >= 7) {
+                System.out.println("Travels with duration greater than 7 days: " + travel);
+            }
+        }
+
+
+        //----------------------------------
+            //Recipes
+    
+        Recipe recipe01 = new Recipe("Tiramisu", 40);
+        Recipe recipe02 = new Recipe("Baked chicken with potatoes and carrots", 120);
+        Recipe recipe03 = new Recipe("Carbona", 30);
+        Recipe recipe04 = new Recipe("Steak with french fries", 20);
+
+        ArrayList<Recipe> listOfRecipes = new ArrayList<>();
+
+        listOfRecipes.add(0, recipe01);
+        listOfRecipes.add(1, recipe02);
+        listOfRecipes.add(2, recipe03);
+        listOfRecipes.add(3, recipe04);
+
+            //Faster preparation recipes
+        for (Recipe recipe : listOfRecipes) {
+            if (recipe.getPreparationTime() <= 29) {
+                System.out.println("Recipes that take less time to prepare: " + recipe);
+            }
+        }
+
+        //----------------------------------
+            //Movies
+        Movie movie01 = new Movie("Harry Potter and the half-blood prince", "Fantasy", 9.9);
+        Movie movie02 = new Movie("K-pop Demon Hunters", "Animation", 8.5);
+        Movie movie03 = new Movie("Ratatouille", "Drama", 10.0);
+        Movie movie04 = new Movie("Pride and prejudice", "Romance", 7.6);
+
+        ArrayList<Movie> listOfMovies = new ArrayList<>();
+
+        listOfMovies.add(0, movie01);
+        listOfMovies.add(1, movie02);
+        listOfMovies.add(2, movie03);
+        listOfMovies.add(3, movie04);
+
+        System.out.println("Full list: " + listOfMovies);
+
+        for (Movie movie : listOfMovies)
+        }
+
+
+    }
+}
---
+++ homeOfficeArrays-main/homeOffice/homeOfficeArrays/src/classes/utilizadas/Product.java
@@ -0,0 +1,36 @@
+package classes.utilizadas;
+
+public class Product {
+    //Attributes
+    private String name;
+    private double price;
+    private Object product01;
+
+    //Constructors
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //Get and set
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nPrice: " + price;
+    }
+}
